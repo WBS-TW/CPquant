3aec8f7a398ddbd1be87992f9ccec13dd62c7723
WBS-TW <37210875+WBS-TW@users.noreply.github.com>
2018-12-10 11:47:04 +0100

    Create server.R

---
+++ server.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(shinythemes)
+library(dplyr)
+library(plotly)
+library(DT)
+library(crosstalk)
+
+
+
+shinyServer(function(input, output, session) {
+  
+  observeEvent(input$go_1, {
+    output$dt_1 <- renderDT({
+      req(input$file_1)
+      tryCatch(
+        {
+        df <- read.csv(input$file_1$datapath,
+                       header = input$header,
+                       sep = input$sep,
+                       quote = input$quote)
+        },
+        error = function(e) {
+          stop(safeError(e))
+          }
+        )
+      if(input$disp == "head") {
+        return(head(df))
+        }
+      })
+    })
+})
+  
