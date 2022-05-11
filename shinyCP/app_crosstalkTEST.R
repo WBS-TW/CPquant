be3c198e40a9c67f36994c6e789ceffb0d1cf703
WBS-TW <37210875+WBS-TW@users.noreply.github.com>
2022-05-11 08:44:14 +0200

    changed plotly to C vs Cl

--- shinyCP/app_crosstalkTEST.R
+++ shinyCP/app_crosstalkTEST.R
@@ -32,20 +32,20 @@ ui <- shiny::navbarPage(
                                         shiny::numericInput("Clmax", "Cl atoms max", value = 15, min = 1, max = 30),
                                         shiny::br(),
                                         selectInput("Adducts", "Add adducts/fragments",
-                                                choices = c("[CP-Cl]-", "[CP-H]-", "[CP-HCl]-", "[CO-Cl]-", "[CO-HCl]-", "[CO-H]-",
-                                                "[CP-Cl-HCl]+", "[CP-Cl-2HCl]+", "[CP-Cl-3HCl]+"),
-                                                selected = "[CP-Cl]-",
-                                                multiple = TRUE,
-                                                selectize = TRUE,
-                                                width = NULL,
-                                                size = NULL),
+                                                    choices = c("[CP-Cl]-", "[CP-H]-", "[CP-HCl]-", "[CO-Cl]-", "[CO-HCl]-", "[CO-H]-",
+                                                                "[CP-Cl-HCl]+", "[CP-Cl-2HCl]+", "[CP-Cl-3HCl]+"),
+                                                    selected = "[CP-Cl]-",
+                                                    multiple = TRUE,
+                                                    selectize = TRUE,
+                                                    width = NULL,
+                                                    size = NULL),
                                         shiny::numericInput("threshold", "Isotope rel ab threshold (%)", value = 5, min = 1, max = 99),
                                         shiny::actionButton("go1", "Submit", width = "100%"),
                                         width = 3),
                                 shiny::mainPanel(
                                         DT::dataTableOutput("Table", width = "100%")
-                                        )
                                 )
+                        )
                         )),
         shiny::tabPanel("Interfering ions",
                         shiny::fluidPage(shiny::sidebarLayout(
@@ -53,15 +53,15 @@ ui <- shiny::navbarPage(
                                         shiny::numericInput("MSresolution", "MS Resolution", value = 60000, min = 100, max = 3000000),
                                         shiny::actionButton("go2", "Calculate", width = "100%"),
                                         width = 2
-                                        ),
+                                ),
                                 shiny::mainPanel(
                                         plotly::plotlyOutput("Plotly"),
                                         plotly::plotlyOutput("Plotly2"),
                                         DT::dataTableOutput("Table2", width = "100%")
                                         
                                 )
-                                )
-                                )),
+                        )
+                        )),
         shiny::tabPanel(
                 "Instructions",
                 shiny::sidebarLayout(
@@ -69,7 +69,7 @@ ui <- shiny::navbarPage(
                                             width = 3),
                         shiny::mainPanel(
                                 shiny::includeMarkdown("instructions.md")
-                                                )
+                        )
                 )
         )
 )
@@ -89,9 +89,9 @@ server = function(input, output, session) {
         selectedAdducts <- eventReactive(input$go1, {as.character(input$Adducts)})
         MSresolution <- eventReactive(input$go2, {as.integer(input$MSresolution)})
         
-
-#----Outputs_Start
-
+        
+        #----Outputs_Start
+        
         CP_allions_glob <- eventReactive(input$go1, {
                 
                 # Create a Progress object
@@ -112,30 +112,30 @@ server = function(input, output, session) {
                 }
                 return(CP_allions)
         })
-              
-                
-                shiny::observeEvent(input$go1, {
+        
+        
+        shiny::observeEvent(input$go1, {
                 output$Table <- DT::renderDT(server=FALSE,{ #need to keep server = FALSE otherwise excel download only part of rows
                         # Show data
                         DT::datatable(CP_allions_glob(), 
-                                  filter = "top", extensions = c("Buttons", "Scroller"),
-                                  options = list(scrollY = 650,
-                                                 scrollX = 500,
-                                                 deferRender = TRUE,
-                                                 scroller = TRUE,
-                                                 buttons = list(list(extend = "excel", title = NULL,
-                                                                     exportOptions = list(
-                                                                             modifier = list(page = "all")
-                                                                     )),
-                                                                list(extend = "colvis", targets = 0, visible = FALSE)),
-                                                 dom = "lBfrtip",
-                                                 fixedColumns = TRUE), 
-                                  rownames = FALSE)
-                        })
+                                      filter = "top", extensions = c("Buttons", "Scroller"),
+                                      options = list(scrollY = 650,
+                                                     scrollX = 500,
+                                                     deferRender = TRUE,
+                                                     scroller = TRUE,
+                                                     buttons = list(list(extend = "excel", title = NULL,
+                                                                         exportOptions = list(
+                                                                                 modifier = list(page = "all")
+                                                                         )),
+                                                                    list(extend = "colvis", targets = 0, visible = FALSE)),
+                                                     dom = "lBfrtip",
+                                                     fixedColumns = TRUE), 
+                                      rownames = FALSE)
                 })
-
-                
-
+        })
+        
+        
+        
         
         shiny::observeEvent(input$go2, {
                 
@@ -149,9 +149,9 @@ server = function(input, output, session) {
                                 difflag == 0 | difflead == 0 ~ FALSE,
                                 reslag >= as.integer(MSresolution()) | reslead >= as.integer(MSresolution()) ~ TRUE,
                                 reslag < as.integer(MSresolution()) & reslead < as.integer(MSresolution()) ~ FALSE
-                                )
-                               )
-
+                        )
+                        )
+                
                 
                 output$Plotly <- plotly::renderPlotly(
                         p <- CP_allions_compl2 %>% plot_ly(
@@ -165,7 +165,7 @@ server = function(input, output, session) {
                                                   '<br>',
                                                   "Adduct/Fragment:", CP_allions_compl2$Fragment))
                         %>% 
-                        plotly::layout(legend=list(title=list(text='<b> Interference at MS res? </b>')))
+                                plotly::layout(legend=list(title=list(text='<b> Interference at MS res? </b>')))
                 )
                 
                 output$Plotly2 <- plotly::renderPlotly(
@@ -185,7 +185,7 @@ server = function(input, output, session) {
                                                   "m/z diff (prev and next):", CP_allions_compl2$difflag, "&", CP_allions_compl2$difflead,
                                                   '<br>',
                                                   "Resolution needed (prev and next):", CP_allions_compl2$reslag, "&", CP_allions_compl2$reslead)
-                                )
+                        )
                         %>% 
                                 plotly::layout(legend=list(title=list(text='<b> Interference at MS res? </b>')))
                 )
@@ -193,36 +193,36 @@ server = function(input, output, session) {
                 output$Table2 <- DT::renderDT(server=FALSE,{ #need to keep server = FALSE otherwise excel download only part of rows
                         # Show data
                         DT::datatable(CP_allions_compl2, 
-                                  filter = "top", extensions = c("Buttons", "Scroller"),
-                                  options = list(scrollY = 650,
-                                                 scrollX = 500,
-                                                 deferRender = TRUE,
-                                                 scroller = TRUE,
-                                                 buttons = list(list(extend = "excel", title = NULL,
-                                                                     exportOptions = list(
-                                                                             modifier = list(page = "all")
-                                                                     )),
-                                                                list(extend = "colvis", targets = 0, visible = FALSE)),
-                                                 dom = "lBfrtip",
-                                                 fixedColumns = TRUE), 
-                                  rownames = FALSE)
+                                      filter = "top", extensions = c("Buttons", "Scroller"),
+                                      options = list(scrollY = 650,
+                                                     scrollX = 500,
+                                                     deferRender = TRUE,
+                                                     scroller = TRUE,
+                                                     buttons = list(list(extend = "excel", title = NULL,
+                                                                         exportOptions = list(
+                                                                                 modifier = list(page = "all")
+                                                                         )),
+                                                                    list(extend = "colvis", targets = 0, visible = FALSE)),
+                                                     dom = "lBfrtip",
+                                                     fixedColumns = TRUE), 
+                                      rownames = FALSE)
                 })
-                })
-        
-#----Outputs_End
-
-        
-        
-        
-       
-        
-# Close the app when the session ends
-if(!interactive()) {
-        session$onSessionEnded(function() {
-                stopApp()
-                q("no")
         })
-}
+        
+        #----Outputs_End
+        
+        
+        
+        
+        
+        
+        # Close the app when the session ends
+        if(!interactive()) {
+                session$onSessionEnded(function() {
+                        stopApp()
+                        q("no")
+                })
+        }
         
 }
 
