46e34f42cf661917113f78b2b10f7f213eec0d45
WBS-TW <37210875+WBS-TW@users.noreply.github.com>
2024-07-23 15:30:40 +0800

    fixed BrCl alkanes interference plot

--- shinyCP/app.R
+++ shinyCP/app.R
@@ -18,7 +18,7 @@ source("./R/getSkyline_BrCl.R")
 #--------------------------------UI function----------------------------------#
 
 ui <- shiny::navbarPage(
-        "PCXplorer",
+        "CPXplorer",
         theme = shinythemes::shinytheme('spacelab'),
         shiny::tabPanel("Initial settings",
                         shiny::fluidPage(shiny::sidebarLayout(
@@ -209,34 +209,63 @@ server = function(input, output, session) {
                 CP_allions_compl2$interference[1] <- FALSE
                 CP_allions_compl2$interference[length(CP_allions_compl2$interference)] <- FALSE
                 
-                # Output scatterplot: #Cl vs #C
+                # Output scatterplot: #Cl vs #C  if Br exists
+                if ("79Br" %in% names(CP_allions_compl2) == TRUE){ 
                 output$Plotly <- plotly::renderPlotly(
                         p <- CP_allions_compl2 %>% 
                                 mutate(`79Br` = tidyr::replace_na(`79Br`, 0)) %>%
                                 mutate(`81Br` = tidyr::replace_na(`81Br`, 0)) %>%
                                 plot_ly(
-                                x = ~ (`12C`+`13C`), 
-                                y = ~(`35Cl`+`37Cl`+`79Br`+`81Br`),
-                                type = "scatter",
-                                mode = "markers",
-                                color = ~interference,
-                                hoverinfo = "text",
-                                hovertext = paste("Parent Formula:", CP_allions_compl2$Parent_Formula,
-                                                  '<br>',
-                                                  "Adduct/Fragment ion:", CP_allions_compl2$Adduct,
-                                                  '<br>',
-                                                  "Ion Formula:", CP_allions_compl2$Adduct_Formula,
-                                                  '<br>',
-                                                  "Adduct isotopes:", paste0("[12C]:", CP_allions_compl2$`12C`, "  [13C]:", CP_allions_compl2$`13C`, 
-                                                                             "  [35Cl]:", CP_allions_compl2$`35Cl`, "  [37Cl]:", CP_allions_compl2$`37Cl`, " [79Br]:", CP_allions_compl2$`79Br`, " [81Br]:", CP_allions_compl2$`81Br`))
-                        )
+                                        x = ~ (`12C`+`13C`), 
+                                        y = ~(`35Cl`+`37Cl`+`79Br`+`81Br`), # need to incorporate Br later
+                                        type = "scatter",
+                                        mode = "markers",
+                                        color = ~interference,
+                                        hoverinfo = "text",
+                                        hovertext = paste("Parent Formula:", CP_allions_compl2$Parent_Formula,
+                                                          '<br>',
+                                                          "Adduct/Fragment ion:", CP_allions_compl2$Adduct,
+                                                          '<br>',
+                                                          "Ion Formula:", CP_allions_compl2$Adduct_Formula,
+                                                          '<br>',
+                                                          "Adduct isotopes:", paste0("[12C]:", CP_allions_compl2$`12C`, "  [13C]:", CP_allions_compl2$`13C`, 
+                                                                                     "  [35Cl]:", CP_allions_compl2$`35Cl`, "  [37Cl]:", CP_allions_compl2$`37Cl`, " [79Br]:", CP_allions_compl2$`79Br`, " [81Br]:", CP_allions_compl2$`81Br`))
+                                )
                         %>% 
                                 plotly::layout(xaxis = list(title = "Number of carbons (12C+13C)"),
                                                yaxis = list(title = "Number of halogens (35Cl+37Cl+79Br+81Br)"),
                                                legend=list(title=list(text='<b> Interference at MS res? </b>')))
                 )
+                } else { #if there are no bromines
+                        output$Plotly <- plotly::renderPlotly(
+                                p <- CP_allions_compl2 %>% 
+                                        plot_ly(
+                                                x = ~ (`12C`+`13C`), 
+                                                y = ~(`35Cl`+`37Cl`),
+                                                type = "scatter",
+                                                mode = "markers",
+                                                color = ~interference,
+                                                hoverinfo = "text",
+                                                hovertext = paste("Parent Formula:", CP_allions_compl2$Parent_Formula,
+                                                                  '<br>',
+                                                                  "Adduct/Fragment ion:", CP_allions_compl2$Adduct,
+                                                                  '<br>',
+                                                                  "Ion Formula:", CP_allions_compl2$Adduct_Formula,
+                                                                  '<br>',
+                                                                  "Adduct isotopes:", paste0("[12C]:", CP_allions_compl2$`12C`, "  [13C]:", CP_allions_compl2$`13C`, 
+                                                                                             "  [35Cl]:", CP_allions_compl2$`35Cl`, "  [37Cl]:", CP_allions_compl2$`37Cl`))
+                                        )
+                                %>% 
+                                        plotly::layout(xaxis = list(title = "Number of carbons (12C+13C)"),
+                                                       yaxis = list(title = "Number of chlorines (35Cl+37Cl)"),
+                                                       legend=list(title=list(text='<b> Interference at MS res? </b>')))
+                        )
+                        
+                }
                 
                 # Output the interference bar plot: Rel_ab vs m/z
+               
+                if ("79Br" %in% names(CP_allions_compl2) == TRUE){ 
                 output$Plotly2 <- plotly::renderPlotly(
                         p <- CP_allions_compl2 %>% plot_ly(
                                 x = ~`m/z`, 
@@ -263,6 +292,35 @@ server = function(input, output, session) {
                         %>% 
                                 plotly::layout(legend=list(title=list(text='<b> Interference at MS res? </b>')))
                 )
+                } else {
+                        output$Plotly2 <- plotly::renderPlotly(
+                                p <- CP_allions_compl2 %>% plot_ly(
+                                        x = ~`m/z`, 
+                                        y = ~Rel_ab,
+                                        type = "bar",
+                                        color = ~interference,
+                                        #text = ~Adduct,
+                                        hoverinfo = "text",
+                                        hovertext = paste("Parent Formula:", CP_allions_compl2$Parent_Formula,
+                                                          '<br>',
+                                                          "Adduct/Fragment ion:", CP_allions_compl2$Adduct,
+                                                          '<br>',
+                                                          "Ion Formula:", CP_allions_compl2$Adduct_Formula,
+                                                          '<br>',
+                                                          "Adduct isotopes:", paste0("[12C]:", CP_allions_compl2$`12C`, "  [13C]:", CP_allions_compl2$`13C`, 
+                                                                                     "  [35Cl]:", CP_allions_compl2$`35Cl`, "  [37Cl]:", CP_allions_compl2$`37Cl`),
+                                                          '<br>',
+                                                          "m/z:", CP_allions_compl2$`m/z`,
+                                                          '<br>',
+                                                          "m/z diff (prev and next):", CP_allions_compl2$difflag, "&", CP_allions_compl2$difflead,
+                                                          '<br>',
+                                                          "Resolution needed (prev and next):", CP_allions_compl2$reslag, "&", CP_allions_compl2$reslead)
+                                )
+                                %>% 
+                                        plotly::layout(legend=list(title=list(text='<b> Interference at MS res? </b>')))
+                        )
+                        
+                }
                 
                 output$Table2 <- DT::renderDT(server=FALSE,{ #need to keep server = FALSE otherwise excel download only part of rows
                         # Show data
